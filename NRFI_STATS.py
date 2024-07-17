2d47f589b5d7698cbf8a3f92f38656d46b9c0343
bena618 <98246992+bena618@users.noreply.github.com>
2024-07-17 02:08:36 -0400

    Update NRFI_STATS.py

--- NRFI_STATS.py
+++ NRFI_STATS.py
@@ -62,109 +62,109 @@ if response.status_code == 200:
               mlb_game_dates.append(date)
   
 
-          # Convert the dates
-          dotw = [elem.split(',')[0] for elem in mlb_game_dates]
-          days = [convert_dates(elem) for elem in mlb_game_dates]
-          teams_last_opp = {}
+  # Convert the dates
+  dotw = [elem.split(',')[0] for elem in mlb_game_dates]
+  days = [convert_dates(elem) for elem in mlb_game_dates]
+  teams_last_opp = {}
 
-          stats = {
-                'Monday': 0,
-                'Tuesday': 0,
-                'Wednesday': 0,
-                'Thursday': 0,
-                'Friday': 0,
-                'Saturday': 0,
-                'Sunday': 0,
-                'Monday_tot': 0,
-                'Tuesday_tot': 0,
-                'Wednesday_tot': 0,
-                'Thursday_tot': 0,
-                'Friday_tot': 0,
-                'Saturday_tot': 0,
-                'Sunday_tot': 0,
-                '1': 0,
-                '2': 0,
-                '3': 0,
-                '4': 0,
-                '1_tot': 0,
-                '2_tot': 0,
-                '3_tot': 0,
-                '4_tot': 0
-          }
-          for i,day in enumerate(days):   
-              url_day = 'https://www.rotowire.com/baseball/scoreboard.php?date=' + day
-              print(url_day)
-          
-          
-              response_day = requests.get(url_day,headers=headers)   
-          
-              if response.status_code == 200:
-                  soup_for_day = BeautifulSoup(response_day.text, 'html.parser')
-                  games = soup_for_day.find_all('div', class_="heading size-1 mb-0")
-          
-                  for game in games:
-                      if game.find('a'):
-          
-                          #team_abbreviations = []
-          
-          
-                          url = 'https://www.rotowire.com' + game.find('a').get('href')
-          
-                          # Set up the Chrome options
-                          chrome_options = webdriver.ChromeOptions()
-                          chrome_options.add_argument('--no-sandbox')
-                          chrome_options.add_argument('--headless')
-          
-                          # Initialize the Chrome driver
-                          driver = webdriver.Chrome(options=chrome_options) 
-          
-                          # Fetch the webpage
-                          driver.get(url)
-          
-                          # Get the HTML content
-                          html = driver.page_source
-          
-                          # Close the driver
-                          driver.quit()
-          
-          
-                          soup_for_game = BeautifulSoup(html, 'html.parser')
-          
-          
-                          if soup_for_game.find('td',class_ ="align-l pad-0-10"):
-          
-                              away_team = soup_for_game.find('td',class_ ="align-l pad-0-10").text
-                              home_team = soup_for_game.find('td',class_ ="pad-0-5 align-l").text
-                              runs_each_inning = soup_for_game.find_all('td',class_ ="pad-0-10")
-          
-                              stats[f"{dotw[i]}_tot"] += 1
-          
-                              if away_team in teams_last_opp and home_team in teams_last_opp:
-                                  if teams_last_opp[away_team][0] == home_team and teams_last_opp[home_team][0] == away_team:
-                                      teams_last_opp[away_team][1] += 1
-                                      teams_last_opp[home_team][1] += 1
-                                  else:
-                                      teams_last_opp[away_team] = [home_team] + [1]
-                                      teams_last_opp[home_team] = [away_team] + [1]
-                              else:
-                                  teams_last_opp[away_team] = [home_team] + [1]
-                                  teams_last_opp[home_team] = [away_team] + [1]
-          
-                              gots = teams_last_opp[home_team][1]
-          
-                              try:
-                                  stats[f'{gots}_tot'] += 1
-                              except:
-                                  print(teams_last_opp[away_team],teams_last_opp[home_team],day)
-          
-          
-                              if runs_each_inning:
-                                  runs_each_inning = [elem.text for elem in runs_each_inning]
-                                  away_team_runs = int(runs_each_inning[1])
-                                  home_team_runs = int(runs_each_inning[int((len(runs_each_inning)/2) + 1)])
-          
-                                  if away_team_runs + home_team_runs == 0:
-                                      stats[dotw[i]] += 1
-                                      stats[f'{gots}'] += 1
-                            
-          print(stats)
+  stats = {
+        'Monday': 0,
+        'Tuesday': 0,
+        'Wednesday': 0,
+        'Thursday': 0,
+        'Friday': 0,
+        'Saturday': 0,
+        'Sunday': 0,
+        'Monday_tot': 0,
+        'Tuesday_tot': 0,
+        'Wednesday_tot': 0,
+        'Thursday_tot': 0,
+        'Friday_tot': 0,
+        'Saturday_tot': 0,
+        'Sunday_tot': 0,
+        '1': 0,
+        '2': 0,
+        '3': 0,
+        '4': 0,
+        '1_tot': 0,
+        '2_tot': 0,
+        '3_tot': 0,
+        '4_tot': 0
+  }
+  for i,day in enumerate(days):   
+      url_day = 'https://www.rotowire.com/baseball/scoreboard.php?date=' + day
+      print(url_day)
+  
+  
+      response_day = requests.get(url_day,headers=headers)   
+  
+      if response.status_code == 200:
+          soup_for_day = BeautifulSoup(response_day.text, 'html.parser')
+          games = soup_for_day.find_all('div', class_="heading size-1 mb-0")
+  
+          for game in games:
+              if game.find('a'):
+  
+                  #team_abbreviations = []
+  
+  
+                  url = 'https://www.rotowire.com' + game.find('a').get('href')
+  
+                  # Set up the Chrome options
+                  chrome_options = webdriver.ChromeOptions()
+                  chrome_options.add_argument('--no-sandbox')
+                  chrome_options.add_argument('--headless')
+  
+                  # Initialize the Chrome driver
+                  driver = webdriver.Chrome(options=chrome_options) 
+  
+                  # Fetch the webpage
+                  driver.get(url)
+  
+                  # Get the HTML content
+                  html = driver.page_source
+  
+                  # Close the driver
+                  driver.quit()
+  
+  
+                  soup_for_game = BeautifulSoup(html, 'html.parser')
+  
+  
+                  if soup_for_game.find('td',class_ ="align-l pad-0-10"):
+  
+                      away_team = soup_for_game.find('td',class_ ="align-l pad-0-10").text
+                      home_team = soup_for_game.find('td',class_ ="pad-0-5 align-l").text
+                      runs_each_inning = soup_for_game.find_all('td',class_ ="pad-0-10")
+  
+                      stats[f"{dotw[i]}_tot"] += 1
+  
+                      if away_team in teams_last_opp and home_team in teams_last_opp:
+                          if teams_last_opp[away_team][0] == home_team and teams_last_opp[home_team][0] == away_team:
+                              teams_last_opp[away_team][1] += 1
+                              teams_last_opp[home_team][1] += 1
+                          else:
+                              teams_last_opp[away_team] = [home_team] + [1]
+                              teams_last_opp[home_team] = [away_team] + [1]
+                      else:
+                          teams_last_opp[away_team] = [home_team] + [1]
+                          teams_last_opp[home_team] = [away_team] + [1]
+  
+                      gots = teams_last_opp[home_team][1]
+  
+                      try:
+                          stats[f'{gots}_tot'] += 1
+                      except:
+                          print(teams_last_opp[away_team],teams_last_opp[home_team],day)
+  
+  
+                      if runs_each_inning:
+                          runs_each_inning = [elem.text for elem in runs_each_inning]
+                          away_team_runs = int(runs_each_inning[1])
+                          home_team_runs = int(runs_each_inning[int((len(runs_each_inning)/2) + 1)])
+  
+                          if away_team_runs + home_team_runs == 0:
+                              stats[dotw[i]] += 1
+                              stats[f'{gots}'] += 1
+                    
+  print(stats)
